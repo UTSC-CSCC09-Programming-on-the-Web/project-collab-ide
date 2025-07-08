5b540080ebe16a8bea245633c368fdb130636a54
crystal / ue <mfihy1@gmail.com>
2025-07-08 14:24:32 -0400

    Remove comment

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,9 +7,13 @@
       >
         Test: Simulate updating stock display!
       </button>
-      <StockDisplay exchange="NASDAQ" ticker="AAPL" // set appropriate ticker
-      :price="currentPrice" :change="priceChange"
-      :percent-change="percentChange" />
+      <StockDisplay
+        exchange="NASDAQ"
+        ticker="AAPL"
+        :price="currentPrice"
+        :change="priceChange"
+        :percent-change="percentChange"
+      />
     </div>
     <ErrorToast
       v-if="isError"
