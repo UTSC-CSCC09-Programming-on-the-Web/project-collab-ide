db2ed8245b1e783096f9ca5ec288b6724e6758db
Tara <92535386+j-tara@users.noreply.github.com>
2025-07-09 15:19:11 -0400

    feat: style title

--- frontend/src/views/SubscriptionView.vue
+++ frontend/src/views/SubscriptionView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="subscription">
-    <h2>Subscribe</h2>
+    <h2 class="font-bebas text-[100px] text-[#1998E1]">SUBSCRIBE</h2>
     <SubscribeButton />
   </div>
 </template>
